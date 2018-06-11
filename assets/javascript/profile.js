6cf0f08f86f82dc4a8bd1ebd08a4fe044fe08697
David L <lapadula.david@gmail.com>
2018-06-10 21:56:18 -0400

    Functions for adding days and API's working

--- assets/javascript/profile.js
+++ assets/javascript/profile.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-
+ 
     //Selectors
         //Display the current temperature
         currentTempDiv = $('#current-temp-div'); 
@@ -7,14 +7,32 @@ $(document).ready(function () {
         //Display the duration of the trip
         startDateProfile = $('#start-date-profile');
         endDateProfile = $('#end-date-profile');
-
+ 
         //Table for displaying the homepage activities list
         profilePageActivities = $('#profile-page-activities');
-
+ 
         //Google map display on the profile page
-        profileMap = $('#profile-map');     
+        profileMap = $('#profile-map'); 
+        
+        // variables to display the weather of the city the user is currently in
+        var userCity; 
+        var userCountry; 
 
-
-
-
+        //function for updating the Weather.
+        var getWeather = function(city, country) {
+            var APIKEY = '&appid=74adba7da79a4c943ca3b1bec036d74d'; 
+            var queryURL = `https://api.openweathermap.org/data/2.5/weather?&units=metric&q=${city},${country}${APIKEY}`; 
+            $.ajax({
+              url : queryURL, 
+              method: 'Get'
+            }).then(function(response){
+              currentTempDiv.text(response.main.temp.toFixed());
+              destinationLat = response.coord.lat; 
+              destinationLon = response.coord.lon; 
+            }); 
+        };
+        getWeather('toronto', 'canada'); //used this to test that the API call works. We can populate this with the user's location
+ 
+  
+    
 }); 
